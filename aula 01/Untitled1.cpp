17b740200a56f400d13abc5f57b0ee59255f5ff4
Gabriel Casagrande <126476989+Bielfer12@users.noreply.github.com>
2024-08-08 21:20:03 -0300

    Add files via upload

---
+++ aula 01/Untitled1.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+	int a = 5;
+	double b = 3.14;
+	char c = 'A';
+	
+	int soma = a + 2;
+	double area = b * a;
+	
+	cout << "Soma:" << soma << endl;
+	cout << "Área:" << area << endl;
+	cout << "Caractere:" << c << endl;
+	 
+}
